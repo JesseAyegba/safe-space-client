57c19faa6446e95ed8f3cfb67714d64191ba7f66
JesseNeon <jessejoseph64@gmail.com>
2022-07-31 14:16:27 +0100

    Installed and configured taiwind css

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "dark-bg": "#0D1117",
+        "brand-purple": "#790675",
+      },
+    },
+  },
+  plugins: [],
+};
